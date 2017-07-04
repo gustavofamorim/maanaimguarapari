a9b6473223fdd12170f95329707bf066fe048fbe
Gustavo Freitas de Amorim <gustavofreitasamorim@gmail.com>
2017-07-04 21:18:36 +0000

    Tela de bem vindo alterada

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,16 +79,9 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Maanaim Guarapari
                 </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                <h4>Em desenvolvimento</h4>
             </div>
         </div>
     </body>
