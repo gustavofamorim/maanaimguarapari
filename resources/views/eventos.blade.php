01696d455a851f874e60a0b02b0b89a87007e836
Gustavo Freitas de Amorim <gustavofreitasamorim@gmail.com>
2017-07-18 14:52:10 +0000

    Criado componente do blade de eventos

--- resources/views/eventos.blade.php
+++ resources/views/eventos.blade.php
@@ -9,22 +9,16 @@
         <p>Aqui estão nossos próximos eventos! Inscreva-se!</p>
     </div>
     <div class="row">
-        <div class="col-sm-6 col-md-4">
-            <div class="thumbnail event">
-                <img class="event-image" src="{{ asset('img/coral_de_jovens.o.svg') }}">
-                <div class="caption event-caption">
-                    <h3 class="event-title">Ensaio com os Jovens - 15/07/17 às 14 horas</h3>
-                    <p>Um trabalho de louvor e adoração a Deus realizado com todos os jovens da Igreja Cristã Maranata de Guarapari.</p>
-                    <p><strong>"Eu vos escrevi, jovens, porque sois fortes, e a palavra de Deus está em vós, e já vencestes o maligno."</strong></p>
-                    <p><strong>1 João 2:14</strong></p>
-                    <p>
-                        <a href="https://docs.google.com/forms/d/e/1FAIpQLSd_CBJ6eYvd1fGCXt8JNZpiIUP9mpLrLt2gf4G5xXAuNwtueg/viewform" target="_blank" class="btn btn-primary" role="button">Inscrever-se</a> 
-                        <a href="https://drive.google.com/drive/folders/0BwuioFg__yU_ZW03V2dxV0lKM0E" target="_blank" class="btn btn-default" role="button">Material</a>
-                    </p>
-                </div>
-            </div>
-        </div>
+        @component('components.events')
+            @slot('title', 'Ensaio com os Jovens')
+            @slot('date', '15/07/17')
+            @slot('time', '14')
+            @slot('image', asset('img/coral_de_jovens.o.svg'))
+            @slot('description', 'Um trabalho de louvor e adoração a Deus realizado com todos os jovens da Igreja Cristã Maranata de Guarapari.')
+            @slot('versicle', '"Eu vos escrevi, jovens, porque sois fortes, e a palavra de Deus está em vós, e já vencestes o maligno." 1 João 2:14')
+            @slot('subscriptionLink', 'https://docs.google.com/forms/d/e/1FAIpQLSd_CBJ6eYvd1fGCXt8JNZpiIUP9mpLrLt2gf4G5xXAuNwtueg/viewform')
+            @slot('materialLink', 'https://drive.google.com/drive/folders/0BwuioFg__yU_ZW03V2dxV0lKM0E')
+        @endcomponent
     </div>
 </div>
-
 @endsection
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -44,20 +44,16 @@
             </div>
             <div class="col-sm-10">
                 <div class="row">
-                  <div class="col-sm-6 col-md-3">
-                    <div class="thumbnail event event-transparent">
-                      <img class="event-image" src="{{ asset('img/coral_de_jovens.o.svg') }}">
-                      <div class="caption event-caption">
-                        <h3 class="event-title">Ensaio com os Jovens - 15/07/17 as 14 horas</h3>
-                        <p>Um trabalho de louvor e adoração a Deus realizado com todos os jovens da Igreja Cristã Maranata de Guarapari.</p>
-                        <p><strong><em>"Eu vos escrevi, jovens, porque sois fortes, e a palavra de Deus está em vós, e já vencestes o maligno." 1 João 2:14</em></strong></p>
-                        <p>
-                            <a href="https://docs.google.com/forms/d/e/1FAIpQLSd_CBJ6eYvd1fGCXt8JNZpiIUP9mpLrLt2gf4G5xXAuNwtueg/viewform" target="_blank" class="btn btn-primary" role="button">Inscrever-se</a> 
-                            <a href="https://drive.google.com/drive/folders/0BwuioFg__yU_ZW03V2dxV0lKM0E" target="_blank" class="btn btn-default" role="button">Material</a>
-                            </p>
-                      </div>
-                    </div>
-                  </div>
+                  @component('components.events')
+                    @slot('title', 'Ensaio com os Jovens')
+                    @slot('date', '15/07/17')
+                    @slot('time', '14')
+                    @slot('image', asset('img/coral_de_jovens.o.svg'))
+                    @slot('description', 'Um trabalho de louvor e adoração a Deus realizado com todos os jovens da Igreja Cristã Maranata de Guarapari.')
+                    @slot('versicle', '"Eu vos escrevi, jovens, porque sois fortes, e a palavra de Deus está em vós, e já vencestes o maligno." 1 João 2:14')
+                    @slot('subscriptionLink', 'https://docs.google.com/forms/d/e/1FAIpQLSd_CBJ6eYvd1fGCXt8JNZpiIUP9mpLrLt2gf4G5xXAuNwtueg/viewform')
+                    @slot('materialLink', 'https://drive.google.com/drive/folders/0BwuioFg__yU_ZW03V2dxV0lKM0E')
+                  @endcomponent
                 </div>
             </div>
         </section>
