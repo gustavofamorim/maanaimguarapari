c2c6c4bf7c7d6393e11e53ab889747f4809d1f60
Gustavo Freitas de Amorim <gustavofreitasamorim@gmail.com>
2017-07-19 13:52:35 +0000

    Acrescentada chave do Google Analytics

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -42,6 +42,14 @@
         @yield('page-content')
         
         @stack('pos-scripts')
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+            ga('create', 'UA-102807200-1', 'auto');
+            ga('send', 'pageview');
+        </script>
         @if (getenv('APP_ENV') === 'local')
             <script id="__bs_script__">
                 //<![CDATA[
